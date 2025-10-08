3ad0643b067103d86f1fcbe921725d7a58f73aba
OrangeNeko <ctx13158034228@outlook.com>
2025-10-09 01:16:51 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register, MessageType
+from astrbot.api.star import Context, Star, register
+from astrbot.api.platform import MessageType
 from astrbot.api import logger
 import astrbot.api.message_components as Comp
 import asyncio
